8626bd06229618dd4c5f8b99f1749b2091c452a9
Ali-Akber Saifee <ali@indydevs.org>
2016-11-30 19:56:52 +0800

    Add example to demonstrate change in behavior of translations inclusions

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -2,12 +2,15 @@ require 'test_helper'
 
 describe Brand do
   before(:each) do
-    @brand = Brand.create
+    @brand = Brand.create(name: 'foo', name_zh_tw: 'bar')
   end
   it 'updates translated attributes' do
     b = Brand.find(@brand.id)
     b.update!(name: 'test', name_zh_tw: 'test2')
     expect(b.reload.name).to eq('test')
     expect(b.reload.name_zh_tw).to eq('test2')
+  end
+  it 'loads all locales in default scope' do
+    expect(Brand.first.translations.size).to eq(2)
   end
 end
--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 RSpec.configure do |config|
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.include Devise::Test::ControllerHelpers, type: :controller
 end
